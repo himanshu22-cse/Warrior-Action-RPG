9be5095306bb9aa107fc68a5aaa39e422a24f145
himanshu22-cse <70806091+himanshu22-cse@users.noreply.github.com>
2024-09-28 02:26:41 +0530

    Added Custom Enhanced Input Component And Added Tags For Data Assets

---
+++ Source/Warrior/Private/DataAssets/Input/DataAsset_InputConfig.cpp
@@ -0,0 +1,16 @@
+// Himanshu Third Project
+
+#include "DataAssets/Input/DataAsset_InputConfig.h"
+
+UInputAction* UDataAsset_InputConfig::FindNativeInputActionByTag(const FGameplayTag& InInputTag)
+{
+	for (const FWarriorInputActionConfig InputActionConfig : NativeInputActions)
+	{
+		if (InputActionConfig.InputTag == InInputTag && InputActionConfig.InputAction)
+		{
+			return InputActionConfig.InputAction;
+		}
+    }
+
+	return nullptr;
+}
---
+++ Source/Warrior/Public/DataAssets/Input/DataAsset_InputConfig.h
@@ -0,0 +1,41 @@
+// Himanshu Third Project
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include"GameplayTagContainer.h"
+#include"DataAsset_InputConfig.generated.h"
+
+class UInputAction;
+class UInputMappingContext;
+
+USTRUCT(BlueprintType)
+struct FWarriorInputActionConfig
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta = (Category ="InputTag"))
+	FGameplayTag InputTag;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	UInputAction* InputAction;
+};
+
+
+UCLASS()
+class WARRIOR_API UDataAsset_InputConfig : public UDataAsset
+{
+	GENERATED_BODY()
+	
+public:
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TObjectPtr<UInputMappingContext>DefaultMappingContext;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta = (TitleProperty = "InputTag"))
+	TArray<FWarriorInputActionConfig>NativeInputActions;
+
+	UInputAction* FindNativeInputActionByTag(const FGameplayTag& InInputTag);
+
+};
