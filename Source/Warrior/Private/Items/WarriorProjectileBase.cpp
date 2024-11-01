02211899688fa4e7d2bca9fa1db7def2481b1ef0
himanshu22-cse <70806091+himanshu22-cse@users.noreply.github.com>
2024-11-01 23:44:13 +0530

    Added Montage for pojectile

--- Source/Warrior/Private/Items/WarriorProjectileBase.cpp
+++ Source/Warrior/Private/Items/WarriorProjectileBase.cpp
@@ -33,7 +33,6 @@ AWarriorProjectileBase::AWarriorProjectileBase()
 
 }
 
-
 void AWarriorProjectileBase::BeginPlay()
 {
 	Super::BeginPlay();
@@ -46,12 +45,7 @@ void AWarriorProjectileBase::BeginPlay()
 
 void AWarriorProjectileBase::OnProjectileHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
 {
-	if (OtherActor)
-	{
-		Debug::Print(OtherActor->GetActorNameOrLabel());
-
-		Destroy();
-	}
+	
 }
 void AWarriorProjectileBase::OnProjectileBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
